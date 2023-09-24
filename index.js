a4936b03dacbb7282da3100e8db2d75786f74a66
Mutakabbir Yeasin <mutakabbir.yeasin@gmail.com>
2023-09-24 14:49:22 +0600

    2nd commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { default: Swal } = require("sweetalert2");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s9op0xi.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,14 +26,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("furnitureDb").collection("users");
     const productCollection = client.db("furnitureDb").collection("product");
     const blogCollection = client.db("furnitureDb").collection("blog");
     const cartCollection = client.db("furnitureDb").collection("cart");
 
+    // user related api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      console.log("Existing user ===>", existingUser);
+      if (existingUser) {
+        return res.send({ message: "User already exist" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // product related api
     app.get("/product", async (req, res) => {
       const result = await productCollection.find().toArray();
       res.send(result);
     });
+
+    // blog related api
     app.get("/blog", async (req, res) => {
       const result = await blogCollection.find().toArray();
       res.send(result);
@@ -52,19 +70,34 @@ async function run() {
     });
 
     app.post("/cart", async (req, res) => {
+      const userId = req?.body?.email; // Assuming you have the user ID available
+      console.log(userId);
       const result = await cartCollection.findOne({
-        productId: req?.body?.productId,
+        userId: userId,
+        productId: req.body.productId,
       });
+
       console.log("result", result);
+
       if (result === null) {
-        const product = req.body;
-        console.log(product);
-        const result = await cartCollection.insertOne(product);
-        res.send(result);
+        const product = {
+          userId: userId,
+          ...req.body,
+        };
+        // console.log(product);
+        const insertResult = await cartCollection.insertOne(product);
+        res.send(insertResult);
       } else {
         console.log("already added this product");
         res.send(false);
       }
+    });
+
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
